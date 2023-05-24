9f7b90aeff42f895d12b5ac51b590266814b18cd
Sarthak Chawathe <sarthak.chawathe2012@gmail.com>
2023-05-24 12:33:17 -0400

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -89,7 +89,7 @@ const config = {
       name: 'Google Data Analytics',
       body: 'Those who earn the Google Data Analytics Professional Certificate have completed eight courses, developed by Google, that include hands-on, practice-based assessments and are designed to prepare them for introductory-level roles in Data Analytics. They are competent in tools and platforms including spreadsheets, SQL, Tableau, and R. They know how to prepare, process, analyze, and share data for thoughtful action.',
       year: 'June 2022',
-      link: 'https://example.com'
+      link: 'https://drive.google.com/file/d/1tXSY1OeE0G3ID2JLbKZ54Zb-4B_xV4rp/view?usp=sharing'
     },
   ],
   education: [
